ad9bbb9a7b856585146390b9cd9a3dc3f82088ef
HuseynovR <tehrza@azercell.com>
2020-02-08 12:34:40 +0400

    First commit updated

--- src/main/java/Begin.java
+++ src/main/java/Begin.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Begin {
     public static void main(String[] args) {
         System.out.print("Hello from Java");
+        System.out.println("Hello,what is your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello." +name);
+        System.out.println("Nice to meet you."+name);
+        System.out.println("Bye");
+
+        System.out.println("How old are you?");
+        int age;
+        age=sc.nextInt();
+        if(age<18)
+            System.out.println("Lets go to sinema");
+        else System.out.println("Lets go to night club");
 
     }
 }
